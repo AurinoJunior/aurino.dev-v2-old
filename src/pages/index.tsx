472e3891820695e4dd461e347cb20db67fe105d1
Aurino Junior <aurinoaj@gmail.com>
2022-02-04 12:35:58 -0300

    Create Container component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,14 @@
 import type { NextPage } from "next";
-import { Logo } from "../components/atoms/Logo";
+
+import { Logo } from "../components/atoms/Logo/Logo";
+import { Container } from "../components/atoms/Container/Container.styles";
 
 const Home: NextPage = () => {
-  return <Logo />;
+  return (
+    <Container>
+      <Logo />
+    </Container>
+  );
 };
 
 export default Home;
