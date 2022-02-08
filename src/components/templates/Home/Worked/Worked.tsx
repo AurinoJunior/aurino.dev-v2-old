5472080fdf01f4b38a88ecace65e53fc17f7ec4c
Aurino Junior <aurinoaj@gmail.com>
2022-02-08 15:17:38 -0300

    Create structure worked partial template

---
+++ src/components/templates/Home/Worked/Worked.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+import { Box } from "./Worked.styles";
+
+export const Worked = () => {
+  return (
+    <Box>
+      <h2>Empresas que trabalhei!</h2>
+      <div>
+        <h2>01</h2>
+        <h3>
+          <span>Catho Online</span>, desenvolvedor web
+        </h3>
+        <p>
+          Desenvolvedor web júnior, responsável pela área logada do candidato no
+          perido de 2 anos e 10 meses.
+        </p>
+      </div>
+      <div>
+        <h2>02</h2>
+        <h3>
+          <span>GetNinjas</span>, desenvolvedor web
+        </h3>
+        <p>
+          Desenvolvedor web júnior, responsável pela área logada do candidato no
+          perido de 2 anos e 10 meses.
+        </p>
+      </div>
+    </Box>
+  );
+};
