3ad04ef483f4018bb2dd601d2b1099485dd1000a
Aurino Junior <aurinoaj@gmail.com>
2022-02-08 14:22:59 -0300

    Add new font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,16 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
+
+          <link
+            href="https://fonts.googleapis.com/css2?family=Sen:wght@400;700&display=swap"
+            rel="stylesheet"
+          />
 
           <link
             href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap"
