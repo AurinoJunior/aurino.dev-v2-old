acf2657ca98da1af08bdb767387e766c7c20d03f
Aurino Junior <aurinoaj@gmail.com>
2022-02-15 14:10:00 -0300

    change footer font-size

--- src/components/atoms/Footer/Footer.styles.ts
+++ src/components/atoms/Footer/Footer.styles.ts
@@ -4,7 +4,7 @@ import { gray_800, secondary } from "../../../styles/Colors";
 export const Paragraph = styled.p`
   text-align: center;
   font-weight: 600;
-  font-size: 1.2rem;
+  font-size: 1rem;
 
   padding: 1rem 0;
   margin-top: 3rem;
