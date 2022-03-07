ed355efac69dc0c345fc8848e83c88596098baf2
Aurino Junior <aurinoaj@gmail.com>
2022-03-07 11:53:58 -0300

    Create structure menu burguer

--- src/components/molecules/Header/Header.styles.ts
+++ src/components/molecules/Header/Header.styles.ts
@@ -1,7 +1,47 @@
 import styled from "styled-components";
-import { gray_800 } from "../../../styles/Tokens";
+import { gray_800, secondary } from "../../../styles/Tokens";
 
 export const Box = styled.div`
   width: 100%;
   padding: 1rem 0;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  button {
+    z-index: 3;
+  }
+`;
+
+export const Nav = styled.nav`
+  background-color: ${gray_800};
+
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 100vh;
+  width: 100%;
+
+  z-index: 2;
+`;
+
+export const NavContent = styled.ul`
+  width: 100%;
+  height: 100%;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 2rem;
+
+  list-style: none;
+
+  font-size: 2rem;
+  font-weight: 600;
+
+  .active {
+    color: ${secondary};
+  }
 `;
--- src/components/molecules/Header/Header.tsx
+++ src/components/molecules/Header/Header.tsx
@@ -1,14 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+import { HiMenu } from "react-icons/hi";
+import { RiCloseLine } from "react-icons/ri";
+
 import { Logo } from "../../atoms/Logo/Logo";
 import { Container } from "../../atoms/Container/Container.styles";
 
-import { Box } from "./Header.styles";
+import { Box, Nav, NavContent } from "./Header.styles";
+
+import { secondary } from "../../../styles/Tokens";
 
 export const Header = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const handleSwitchOpenMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
     <Container>
       <Box>
         <Logo />
+
+        <button type="button" onClick={handleSwitchOpenMenu}>
+          {!showMenu ? (
+            <HiMenu size={24} color={secondary} />
+          ) : (
+            <RiCloseLine className="nav__icon" size={24} color={secondary} />
+          )}
+        </button>
+
+        {showMenu && (
+          <Nav>
+            <NavContent>
+              <li className="active">
+                <a href="#">Home</a>
+              </li>
+              <li>
+                <a href="#">Blog</a>
+              </li>
+              <li>
+                <a href="#">Viagens</a>
+              </li>
+            </NavContent>
+          </Nav>
+        )}
       </Box>
     </Container>
   );
