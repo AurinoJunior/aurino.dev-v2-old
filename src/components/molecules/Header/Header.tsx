687a400d1229b5525d4fe69c07a5383a986dbf01
Aurino Junior <aurinoaj@gmail.com>
2022-03-07 16:22:57 -0300

    Fix header menu

--- src/components/molecules/Header/Header.tsx
+++ src/components/molecules/Header/Header.tsx
@@ -1,49 +1,15 @@
-import React, { useState } from "react";
-import { HiMenu } from "react-icons/hi";
-import { RiCloseLine } from "react-icons/ri";
-
 import { Logo } from "../../atoms/Logo/Logo";
 import { Container } from "../../atoms/Container/Container.styles";
 
-import { Box, Nav, NavContent } from "./Header.styles";
-
-import { secondary } from "../../../styles/Tokens";
+import { Box } from "./Header.styles";
+import { NavHeader } from "./NavHeader/NavHeader";
 
 export const Header = () => {
-  const [showMenu, setShowMenu] = useState(false);
-
-  const handleSwitchOpenMenu = () => {
-    setShowMenu(!showMenu);
-  };
-
   return (
     <Box>
       <Container>
         <Logo />
-
-        <button type="button" onClick={handleSwitchOpenMenu}>
-          {!showMenu ? (
-            <HiMenu size={28} color={secondary} />
-          ) : (
-            <RiCloseLine size={28} color={secondary} />
-          )}
-        </button>
-
-        {showMenu && (
-          <Nav>
-            <NavContent>
-              <li className="active">
-                <a href="#">Home</a>
-              </li>
-              <li>
-                <a href="#">Blog</a>
-              </li>
-              <li>
-                <a href="#">Viagens</a>
-              </li>
-            </NavContent>
-          </Nav>
-        )}
+        <NavHeader />
       </Container>
     </Box>
   );
