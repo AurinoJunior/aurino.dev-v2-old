eecf874ccf19450d0fbbb9077e25e294b19c1af6
Aurino Geraldo <aurinoaj@gmail.com>
2022-04-12 22:43:42 -0300

    Refactor About component

--- src/components/Home/About/About.styles.ts
+++ src/components/Home/About/About.styles.ts
@@ -1,20 +1,7 @@
 import styled from "styled-components";
 import { gray_500, tablet_size } from "../../../styles/Tokens";
 
-export const SubTitle = styled.h3`
-  margin: 2rem 0;
-  font-style: italic;
-  color: ${gray_500};
-  font-weight: 400;
-`;
-
-export const Paragraph = styled.p`
-  & + p {
-    margin-top: 1rem;
-  }
-`;
-
-export const Box = styled.div`
+export const AboutBox = styled.div`
   padding-top: 6rem;
 
   @media (min-width: ${tablet_size}) {
@@ -24,5 +11,18 @@ export const Box = styled.div`
     & > div {
       flex: 1;
     }
+  }
+`;
+
+export const AboutQuote = styled.blockquote`
+  margin: 2rem 0;
+  font-style: italic;
+  color: ${gray_500};
+  font-weight: 400;
+`;
+
+export const AboutParagraph = styled.p`
+  & + p {
+    margin-top: 1rem;
   }
 `;
--- src/components/Home/About/About.tsx
+++ src/components/Home/About/About.tsx
@@ -2,34 +2,34 @@ import React from "react";
 
 import { Skillset } from "./Skillset/Skillset";
 
-import { SubTitle, Paragraph, Box } from "./About.styles";
+import { AboutQuote, AboutBox, AboutParagraph } from "./About.styles";
 
 export const About = () => {
   return (
-    <Box id="about">
+    <AboutBox id="about">
       <div>
         <h2>
           Salve galera! <br />
           Aurino Junior aqui.
         </h2>
-        <SubTitle>
+        <AboutQuote>
           Não importa quanto tempo você tem, mas como você o usa. - Ekko
-        </SubTitle>
-        <Paragraph>
+        </AboutQuote>
+        <AboutParagraph>
           Atualmente sou desenvolvedor web front-end no GetNinjas, me formei em
           ciência da computação em 2019 e desde então tenho desbravado esse
           mundo da programação web, metodologias ágeis e cultura devops.
-        </Paragraph>
-        <Paragraph>
+        </AboutParagraph>
+        <AboutParagraph>
           Possuo experiência em teste AB, criação de design system, melhoria de
           métricas em web core vitals e criação de testes automatizados.
-        </Paragraph>
-        <Paragraph>
+        </AboutParagraph>
+        <AboutParagraph>
           Bom, espero conseguir compartilhar um pouco dos meus estudos com vocês
           e aprender bastante durantes esse processo, bora aprender juntos 🚀
-        </Paragraph>
+        </AboutParagraph>
       </div>
       <Skillset />
-    </Box>
+    </AboutBox>
   );
 };
