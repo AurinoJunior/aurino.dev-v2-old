c02de4c732d885e5c2159a815d0c36e886ec9bc6
Aurino Geraldo <aurinoaj@gmail.com>
2022-07-16 11:20:49 -0300

    Add GA

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,24 @@
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
 
 import { GlobalStyle } from '../styles/GlobalStyles'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-V2LBQB6TKD"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-V2LBQB6TKD');
+        `}
+      </Script>
       <GlobalStyle />
       <Component {...pageProps} />
     </>
