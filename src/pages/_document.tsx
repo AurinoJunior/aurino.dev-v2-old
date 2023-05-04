69265888d116da3c66e38596816090e7d2aedb85
Aurino Geraldo <aurinoaj@gmail.com>
2023-05-04 12:50:38 -0300

    Update project version

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -45,7 +45,7 @@ export default class MyDocument extends Document {
           <link
             rel="preconnect"
             href="https://fonts.gstatic.com"
-            crossOrigin="true"
+            crossOrigin="anonymous"
           />
 
           <link
