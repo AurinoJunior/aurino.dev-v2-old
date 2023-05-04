fce6ad91ae25c4257c48550369ffceaf3f7fb1cd
Aurino Geraldo <aurinoaj@gmail.com>
2023-05-04 12:02:04 -0300

    Remove date-fns

--- src/utils/formatDistanceDate.ts
+++
@@ -1,9 +0,0 @@
-import { formatDistance } from 'date-fns'
-import { ptBR } from 'date-fns/locale'
-
-export function formatDistanceDate(date: string) {
-  return formatDistance(new Date(date), new Date(), {
-    locale: ptBR,
-    addSuffix: true
-  })
-}
