7b7bd932ac8a2d1a3d1a634e1e902f6b11764c89
Aurino Geraldo <aurinoaj@gmail.com>
2023-07-04 17:04:55 -0300

    prepare page for blog

---
+++ src/pages/blog.tsx
@@ -0,0 +1,24 @@
+import type { NextPage } from 'next'
+
+import { ErrorPage } from '../components/ErrorPage/ErrorPage'
+
+import errorContent from '../data/errorContent.json'
+
+import CatMaintenance from '../assets/cat-maintenance.png'
+
+const Blog: NextPage = () => {
+  const { maintenance } = errorContent
+
+  return (
+    <>
+      <ErrorPage
+        data={{
+          ...maintenance,
+          imageData: { src: CatMaintenance, alt: 'Um gato descolado' }
+        }}
+      />
+    </>
+  )
+}
+
+export default Blog
