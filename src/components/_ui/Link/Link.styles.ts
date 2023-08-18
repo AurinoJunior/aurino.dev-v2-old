00c656fee79bd21361803b315de64fe37aff6376
Aurino Geraldo <aurinoaj@gmail.com>
2023-08-17 22:07:38 -0300

    Fix padding link component

--- src/components/_ui/Link/Link.styles.ts
+++ src/components/_ui/Link/Link.styles.ts
@@ -3,17 +3,20 @@ import styled from 'styled-components'
 import { gray_800, primary, secondary, white } from '../../../styles/Tokens'
 
 export const Link = styled.div`
-  display: block;
   background-color: ${primary};
 
   border-radius: 0.5rem;
 
-  padding: 0.75rem 1rem;
   width: fit-content;
 
   font-weight: 600;
   text-align: center;
   color: ${gray_800};
+
+  a {
+    display: block;
+    padding: 0.75rem 1rem;
+  }
 
   &.secondary-btn {
     background-color: ${secondary};
@@ -21,5 +24,9 @@ export const Link = styled.div`
 
   &.white-btn {
     background-color: ${white};
+  }
+
+  &:hover {
+    filter: brightness(0.7);
   }
 `
